3cebdae3c647c001275db24f0dd7691e3d8ca0ad
abhishek singh <44000178+iabhishek15@users.noreply.github.com>
2021-08-13 10:12:48 +0530

    Update key.py

--- key.py
+++ key.py
@@ -1,9 +1,8 @@
-youtube_key = "AIzaSyBcVSMbdxy6xrujJh5IAWUAvIajhngqqpE"
-youtube_url = "https://www.googleapis.com/ysoutube/v3/activities?key=AIzaSyBcVSMbdxy6xrujJh5IAWUAvIajhngqqpE"
-
-url = "https://www.youtube.com/playlist?list=PL1dLU3zjLr72ALHXlx04ANJErW0jG-QjE"
-spotify_client_id =  "9156d6f9814145d1b4737493aeb96884"
-spotify_app_client_secret_id = "dfae4eab707e48ce8335d2b37df6d6e9"
-spotify_auth_token = "BQBiCDHmQp4J-N3dCNuhjFSon-nT72bVMAaix7mZjL9F0cdM0XelKWePNXE-YVQ4WvBPMf87xa9CEgYD0w9oXuKiWCIW_o0xLksSg6rouOmjE0i6jcJvbNYrW0s-PPjBuCysSgQ47dz-HNjaneEM-o2VxTMw0KOEWO9tGSulDSsB72sEB-2NyyykegT83wyvZ7ZUjH83bxfM3G9uDEwyc1QTwfh2LEiZVmucNrr1S72TeJVyDl6f9qP1fUeMfMvbpxEYMQyEcaCGJ4B3O_Tg5n3CK7nlV-PIQ8M"
+youtube_key = ""
+youtube_url = ""
+url = ""
+spotify_client_id =  ""
+spotify_app_client_secret_id = ""
+spotify_auth_token = ""
 
 			
